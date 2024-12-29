c21a7b55c0511ad42b4db02b559d419dc534116e
Nuno Madaleno <nuno.madaleno@outlook.com>
2024-12-29 21:52:39 +0000

    experimentar setTimeout e formato de logs

--- a.js
+++ a.js
@@ -1,1 +1,31 @@
-console.log('Hello World!')
+console.log("Hello World!");
+
+const date = new Date();
+console.log(date.getDate());
+console.log(date.getMonth());
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+
+console.log(
+  date.getDate(),
+  date.getMonth(),
+  date.getFullYear(),
+  date.getHours(),
+  date.getMinutes(),
+  date.getSeconds()
+);
+
+console.log(
+  date.getDate(),
+  date.getMonth(),
+  date.getFullYear(),
+  date.getHours(),
+  date.getMinutes(),
+  date.getSeconds()
+);
+
+console.log(
+  `[${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]`
+);
--- b.js
+++ b.js
@@ -12,3 +12,37 @@ function addCart(p) {
   cart = {};
   return cart;
 }
+
+const date = new Date()
+
+setTimeout(() => {
+  console.log(
+    `[${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] - Set time out 1`
+  );
+  // console.log("Set time out 1");
+}, 1000);
+
+setTimeout(() => {
+    console.log(
+        `[${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] - Set time out 2`
+      );
+  // console.log("Set time out 2");
+}, 2000);
+setTimeout(() => {
+    console.log(
+        `[${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] - Set time out 3`
+      );
+  // console.log("Set time out 3");
+}, 3000);
+setTimeout(() => {
+    console.log(
+        `[${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] - Set time out 4`
+      );
+  // console.log("Set time out 4");
+}, 4000);
+setTimeout(() => {
+    console.log(
+        `[${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] - Set time out 5`
+      );
+  // console.log("Set time out 5");
+}, 5000);
